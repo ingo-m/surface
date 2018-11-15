a1079b14493ac4ad7e798ba444cf6ad85885c560
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-11-15 19:25:18 +0100

    Re-run depth sampling.

--- analysis/hyperscript.sh
+++ analysis/hyperscript.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-source /home/john/PhD/GitLab/surface/analysis/20181105/metascript_01.sh &> /home/john/Dropbox/Sonstiges/surface_log_20181105.txt
-
-source /home/john/PhD/GitLab/surface/analysis/20181107/metascript_01.sh &> /home/john/Dropbox/Sonstiges/surface_log_20181107.txt
-
-source /home/john/PhD/GitLab/surface/analysis/20181108/metascript_01.sh &> /home/john/Dropbox/Sonstiges/surface_log_20181108.txt
+source /home/john/PhD/GitLab/surface/analysis/20181029/metascript_03.sh
+source /home/john/PhD/GitLab/surface/analysis/20181105/metascript_03.sh
+source /home/john/PhD/GitLab/surface/analysis/20181107/metascript_03.sh
+source /home/john/PhD/GitLab/surface/analysis/20181108/metascript_03.sh
